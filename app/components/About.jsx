37785dbfc9911d65062a23a1e1e534007415ff4a
bklynate <nathanielcfa@gmail.com>
2016-07-06 18:12:25 -0400

    refactor

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,13 +1,5 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: () => {
-//     return (
-//       <h1>This is the About component</h1>
-//     );
-//   }
-// });
-
 var About = (props) => {
   return(
     <h1>This is the ABOUT component</h1>
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,13 +1,5 @@
 var React = require('react');
 
-// var Examples = React.createClass({
-//   render: () => {
-//     return (
-//       <h1>This is the Examples component</h1>
-//     );
-//   }
-// });
-
 var Examples = (props) => {
   return(
     <h1>This is the EXAMPLES component</h1>
