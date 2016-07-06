b6ffb66a0006f6826f2a2e331db6a15bf90853ac
bklynate <nathanielcfa@gmail.com>
2016-07-06 18:38:39 -0400

    refactor

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,8 +1,13 @@
 var React = require('react');
 
-var WeatherMessage = (props) => {
-  var {temp, location} = props;
+// var WeatherMessage = (props) => {
+//   var {temp, location} = props;
+//   return(
+//     <h3>{temp}&#8457; in {location}</h3>
+//   );
+// }
 
+var WeatherMessage = ({temp, location}) => {
   return(
     <h3>{temp}&#8457; in {location}</h3>
   );
