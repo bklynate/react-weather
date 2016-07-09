c7d3dbb4a37115bd2bca5e75ac0163101ffe08f6
bklynate <nathanielcfa@gmail.com>
2016-07-09 06:05:41 -0400

    add style changes to examples and main components

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return(
     <div>
-      <h1>Examples</h1>
-      <p>Welcome from the examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a couple locations to try out: </p>
+      <ol>
+        <li>
+          <Link to='/?location=Brooklyn'>Brooklyn, NY</Link>
+        </li>
+        <li>
+          <Link to='/?location=Dallas'>Dallas, TX</Link>
+        </li>
+      </ol>
     </div>
   );
 }
