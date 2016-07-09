8ad691dea99e975328a4477bf882ab0a18b025f0
bklynate <nathanielcfa@gmail.com>
2016-07-09 03:50:08 -0400

    add css styling with foundation

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,12 +1,5 @@
 var React = require('react');
 
-// var WeatherMessage = (props) => {
-//   var {temp, location} = props;
-//   return(
-//     <h3>{temp}&#8457; in {location}</h3>
-//   );
-// }
-
 var WeatherMessage = ({temp, location}) => {
   return(
     <h3>{temp}&#8457; in {location}</h3>
